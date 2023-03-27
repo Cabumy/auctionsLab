dffa0192096e0eb3b0cf88b9d1ed5734232601b9
Dragos Olteanu <dragons.olteanu@polofermigiorgi.it>
2023-03-27 10:42:41 +0200

    Puppa forte

--- src/Server.java
+++ src/Server.java
@@ -6,6 +6,7 @@ public class Server {
         try (ServerSocket ss = new ServerSocket(8080);){
             Socket client = ss.accept();
             System.out.println("Pippo");
+            System.out.println("Parigi");
         } catch (Exception e) {
             e.printStackTrace();
         }
