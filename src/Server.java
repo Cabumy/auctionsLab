dec9ebfb8140fec06b33a82e9f0b7e9c624babc8
Dragos Olteanu <dragons.olteanu@polofermigiorgi.it>
2023-03-27 10:44:21 +0200

    puppa

--- src/Server.java
+++ src/Server.java
@@ -5,8 +5,6 @@ public class Server {
     public static void main(String[] args) {
         try (ServerSocket ss = new ServerSocket(8080);){
             Socket client = ss.accept();
-            System.out.println("Pippo");
-            System.out.println("Parigi");
         } catch (Exception e) {
             e.printStackTrace();
         }
