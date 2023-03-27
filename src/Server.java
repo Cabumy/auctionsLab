1221eae3b266db87a2eae8598dad215907173da7
Dragos Olteanu <dragons.olteanu@polofermigiorgi.it>
2023-03-27 10:40:49 +0200

    Pippo

--- src/Server.java
+++ src/Server.java
@@ -5,6 +5,7 @@ public class Server {
     public static void main(String[] args) {
         try (ServerSocket ss = new ServerSocket(8080);){
             Socket client = ss.accept();
+            System.out.println("Pippo");
         } catch (Exception e) {
             e.printStackTrace();
         }
